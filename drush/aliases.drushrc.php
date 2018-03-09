1a538e8939655803e285f004bc522c107b53d9d9
Tim Broeker <tim@electriccitizen.com>
2018-03-09 15:38:19 -0600

    circle config'

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -1,17 +1,10 @@
 <?php
-$aliases['newupstream.dev'] = array(
-  'uri' => 'dev-newupstream.pantheonsite.io',
-  'db-url' => 'mysql://pantheon:a6872daf0abe4ecbabac7d185e6d8e82@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:12588/pantheon',
-  'db-allows-remote' => TRUE,
-  'remote-host' => 'appserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
-  'remote-user' => 'dev.8be50dbc-4463-43b5-bd18-d83046623820',
-  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
-  'path-aliases' => array(
-    '%files' => 'code/sites/default/files',
-    '%drush-script' => 'drush',
-  ),
+$aliases['ecd8.dev'] = array(
+  'remote-host' => '192.241.140.130',
+  'remote-user' => 'master_mvbsuzfhag',
+  'root' => '/home/master/applications/rbzsucbakn/public_html/web',
+  'uri' => 'phpstack-156516-451575.cloudwaysapps.com',
 );
-
 $aliases['ecd8.local'] = array(
   'uri' => 'ecd8.local',
   'root' => '/var/www/drupalvm/web',
